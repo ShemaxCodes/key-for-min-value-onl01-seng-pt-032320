8b1d44451e7cb6dc0acbe69956b7517cd7941fe6
Jashema Panapa <jcpanapa@gmail.com>
2020-04-02 04:55:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,8 @@
 def key_for_min_value(name_hash)
 key_with_smallest_value = 0 
 name_hash.collect do |key, value| 
-  if key  
+  if key == key_with_smallest_value += 1 
+    puts key 
+  else 
   end
 end
