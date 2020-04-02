7bd7a21a7d7c02730dbe8da89d1b1e9b702534ec
Jashema Panapa <jcpanapa@gmail.com>
2020-04-02 05:10:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-key_with_smallest_value = 0 
+smallest_value = 0 
 name_hash.collect do |key, value| 
   if value == key_with_smallest_value += 1 
     puts
